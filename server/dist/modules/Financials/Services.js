7f319ec7906caedc1d07839244419c3a9ee3c1bc
Ayannamdeo <ayannamdeodesk@gmail.com>
2024-09-23 22:08:26 +0530

    commit after git course

--- server/dist/modules/Financials/Services.js
+++ server/dist/modules/Financials/Services.js
@@ -11,41 +11,77 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
+var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.FinancialServices = void 0;
 const fast_csv_1 = require("fast-csv");
 const fs_1 = __importDefault(require("fs"));
 const Repository_1 = require("./repositories/Repository");
+const csvDataValidation_1 = require("../../lib/middlewares/csvDataValidation");
 class FinancialServices {
     constructor() {
         this.getAllFinancials = () => __awaiter(this, void 0, void 0, function* () {
             return yield this.financialRepository.getAll();
         });
+        this.processInChunks = (data) => __awaiter(this, void 0, void 0, function* () {
+            const chunkSize = 100000;
+            const totalSize = data.length;
+            for (let i = 0; i < totalSize; i += chunkSize) {
+                const chunk = data.slice(i, i + chunkSize);
+                yield this.financialRepository.uploadCsv(chunk);
+            }
+        });
         this.uploadCsvFile = (fileName, filePath) => __awaiter(this, void 0, void 0, function* () {
-            // const dataToInsert: any[] = [];
-            // const validData: any[] = [];
-            // const invalidData: any[] = [];
-            // const startTime: string = new Date().toLocaleString();
-            // const uploadId = new Date().getTime().toString();
+            const dataToInsert = [];
+            const startTime = new Date().toLocaleString();
             fs_1.default.createReadStream(filePath)
                 .pipe((0, fast_csv_1.parse)({ headers: true }))
                 .on("error", (error) => console.log(error))
                 .on("data", (row) => {
-                const transformedRow = {
-                    name: row.name,
-                    age: parseInt(row.age, 10), // Convert age to a number
-                    sex: row.sex,
-                    city: row.city,
-                    accountNumber: row.accountNumber, // Keep accountNumber as a string it's an identifier
-                    accountName: row.accountName,
-                    amount: row.amount,
-                    currencyName: row.currencyName,
-                };
-                console.log("inside createStream parsing the rows: ", transformedRow);
+                const transformedData = _a.transformEntry(row);
+                dataToInsert.push(transformedData);
+                console.log("inside createStream parsing the rows: ", transformedData);
             })
-                .on("end", (rowCount) => console.log(`Parsed ${rowCount} rows`));
+                .on("end", (rowCount) => __awaiter(this, void 0, void 0, function* () {
+                const { passed, failed } = yield _a.validateEntries(dataToInsert);
+                const reponse = yield this.processInChunks(passed);
+                console.log(`Parsed ${rowCount} rows`);
+            }));
         });
         this.financialRepository = new Repository_1.FinancialRepository();
     }
 }
 exports.FinancialServices = FinancialServices;
+_a = FinancialServices;
+FinancialServices.transformEntry = (csvRowData) => {
+    const transformedRow = {
+        name: csvRowData.name,
+        age: parseInt(csvRowData.age, 10), // Convert age to a number
+        sex: csvRowData.sex,
+        city: csvRowData.city,
+        accountNumber: csvRowData.accountNumber, // Keep accountNumber as a string it's an identifier
+        accountName: csvRowData.accountName,
+        amount: csvRowData.amount,
+        currencyName: csvRowData.currencyName,
+    };
+    return transformedRow;
+};
+FinancialServices.validateEntries = (entries) => __awaiter(void 0, void 0, void 0, function* () {
+    const passed = [];
+    const failed = [];
+    const uploadId = new Date().getTime().toString();
+    entries.forEach((entry, index) => {
+        const { error } = csvDataValidation_1.CsvDataValidation.FinancialDetailSchema.validate(entry, { abortEarly: false });
+        if (error) {
+            failed.push({
+                uploadId,
+                rowNumber: index + 1,
+                errorDetails: error === null || error === void 0 ? void 0 : error.details.map((err) => `${err.message}\n`),
+            });
+        }
+        else {
+            passed.push(entry);
+        }
+    });
+    return { passed, failed };
+});
